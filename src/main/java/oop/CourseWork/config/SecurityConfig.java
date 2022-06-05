9897f89b8f224713892be5496e075d827af16062
GrEFeRFeeD <azofer77@gmail.com>
2022-06-05 12:51:36 +0300

    Provided temporary index.html

--- src/main/java/oop/CourseWork/config/SecurityConfig.java
+++ src/main/java/oop/CourseWork/config/SecurityConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -37,7 +36,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/login").permitAll()
+                .antMatchers("/login", "/").permitAll()
                 .antMatchers("/checks/**").hasAnyRole("CASHIER", "MANAGER")
                 .antMatchers("/receivings/**").hasAnyRole("RECEIVER", "MANAGER")
                 .antMatchers(HttpMethod.GET, "/orders/**").hasAnyRole("MANAGER")
